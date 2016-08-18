c2e26bbc78ffaeb277c68e2b40b51e781a7a3952
tomateba <t.tebaldi@unitn.it>
2016-08-19 00:55:33 +0200

    update documentation

---
+++ R/p53retriever.R
@@ -0,0 +1,8 @@
+#'  detection and display of p53 putative response elements (REs) on DNA sequences
+#'
+#'p53retriever implements a pattern search algorithm that maps p53
+#'response elements (REs) and ranks them according to predicted transactivation
+#'potentials in five classes. Besides canonical, full site REs, p53tracker
+#'contains specific pattern searches for non-canonical half sites and 3/4 sites.
+#'REs can be displayed along the input sequence in a graphical layout.
+"_PACKAGE"
